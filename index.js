f528206e65e01cddb6ef39949e9f051c74d278b8
BoubaWade <boups12@hotmail.com>
2023-01-23 02:59:52 +0100

    update

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const navList = document.getElementById("nav-list");
-const main = document.querySelector(".main");
-const nbaContainer = document.querySelector(".nba-container");
-const proAContainer = document.querySelector(".pro-a-container");
-const headerContainerH1 = document.querySelector(".header-container > h1");
+// const main = document.querySelector(".main");
+// const nbaContainer = document.querySelector(".nba-container");
+// const proAContainer = document.querySelector(".pro-a-container");
+// const headerContainerH1 = document.querySelector(".header-container > h1");
 
-const arrow1 = document.querySelector(".arrow-1");
-const arrow3 = document.querySelector(".arrow-3");
-const arrow2 = document.querySelector(".arrow-2");
-const arrow4 = document.querySelector(".arrow-4");
+// const arrow1 = document.querySelector(".arrow-1");
+// const arrow3 = document.querySelector(".arrow-3");
+// const arrow2 = document.querySelector(".arrow-2");
+// const arrow4 = document.querySelector(".arrow-4");
 
 window.addEventListener("load", () => {
   navList.style.transform = "translateX(0)";
 });
 
-arrow1.addEventListener("click", () => {
-  main.style.transform = "translateY(-1500px)";
-  nbaContainer.style.transform = "translateY(-785px)";
-  navList.style.transition = "0.05s";
-  navList.style.visibility = "hidden";
-});
-arrow2.addEventListener("click", () => {
-  main.style.transform = "translateY(0)";
-  nbaContainer.style.transform = "translateY(0)";
-  navList.style.transitionDelay = "1s";
-  navList.style.visibility = "visible";
-});
+// arrow1.addEventListener("click", () => {
+//   main.style.transform = "translateY(-1500px)";
+//   nbaContainer.style.transform = "translateY(-785px)";
+//   navList.style.transition = "0.05s";
+//   navList.style.visibility = "hidden";
+// });
+// arrow2.addEventListener("click", () => {
+//   main.style.transform = "translateY(0)";
+//   nbaContainer.style.transform = "translateY(0)";
+//   navList.style.transitionDelay = "1s";
+//   navList.style.visibility = "visible";
+// });
 
-arrow3.addEventListener("click", () => {
-  nbaContainer.style.transform = "translateY(-1610px)";
-  proAContainer.style.transform = "translateY(-1610px)";
-});
-arrow4.addEventListener("click", () => {
-  nbaContainer.style.transform = "translateY(-785px)";
-  proAContainer.style.transform = "translateY(0)";
-});
+// arrow3.addEventListener("click", () => {
+//   nbaContainer.style.transform = "translateY(-1610px)";
+//   proAContainer.style.transform = "translateY(-1610px)";
+// });
+// arrow4.addEventListener("click", () => {
+//   nbaContainer.style.transform = "translateY(-785px)";
+//   proAContainer.style.transform = "translateY(0)";
+// });
